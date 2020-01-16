47e46afac3138a7cc12355bc31a77b56617289c1
muftah <muftah.elzawi@gmail.com>
2020-01-16 12:00:42 +0200

    clint-side-commponent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,29 @@
+// import { AngularFireModule } from 'angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { AngularFireModule } from 'angular/fire'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent
   ],
   imports: [
     BrowserModule,
+    // AngularFireModule.initalizeApp(),
     AppRoutingModule
   ],
   providers: [],
