b60cad4ce7840d485d9a4bfca44ce3dfe059dfe7
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-01-16 11:11:26 +0100

    uno

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SerachBarComponent } from './serach-bar/serach-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SerachBarComponent
   ],
   imports: [
     BrowserModule,
